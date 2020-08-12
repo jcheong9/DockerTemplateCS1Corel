5977b7973d33909b06f7c1f9620cfb7f2970c994
kerrCom <User@DESKTOP-U9O9H5V>
2020-08-12 15:03:59 -0700

    fixed mach sys zone input field

--- DockerTemplateCS1test/DockerUI.xaml.cs
+++ DockerTemplateCS1test/DockerUI.xaml.cs
@@ -134,7 +134,7 @@ namespace DockerTemplateCS1test
                 btn_MachProZone.Click += (s, e) => {
                     int countLabel = 0;
                     //23 xls / 10 = 2.3 round up = 3 - 1 = 2 
-                    string[] filePaths = Directory.GetFiles(@"F:\pointlist\machprozone");
+                    string[] filePaths = Directory.GetFiles(@"E:\pointlist\machprozone");
                     //Pages pages = this.corelApp.ActiveDocument.Pages;
                     //Layers allLayers = pages[3].Layers;
                     ////copy and paste to other pages
@@ -149,12 +149,12 @@ namespace DockerTemplateCS1test
 
                     Microsoft.Office.Interop.Excel.Application xl = new Microsoft.Office.Interop.Excel.Application();
                     //for (int i = 0; i < filePaths.Length; i++)
+                    double xI = 2.17, xO, yI = 8.35, yO = 8.4, xSS, ySS = 8.46;
                     for (int i = 0; i < 10; i++)
                     {
                         Microsoft.Office.Interop.Excel.Workbook workbook = xl.Workbooks.Open(@filePaths[i]);
                         Microsoft.Office.Interop.Excel.Worksheet sheet = workbook.Sheets[1];
                         int numRowsInput = sheet.UsedRange.Rows.Count;
-                        double xI, xO, yI = 8.46, yO = 8.4, xSS, ySS = 8.46;
 
 
                         Excel.Range cellPanelNumber;
@@ -163,20 +163,32 @@ namespace DockerTemplateCS1test
                         Excel.Range cellInOut;
                         Excel.Range cellLabel;
                         String strInOut;
-                        xI = 2.48;
-                        for (int rowIndex = 5; rowIndex <= numRowsInput; rowIndex++)
+
+                        if (countLabel >= 5)
+                        {
+                            xI = 6.265;
+                        }
+                        else{
+                            xI = 2.48;
+                        }
+
+                        if (countLabel == 5)
+                        {
+                            yI = 8.35;
+                            yO = 8.4;
+                            ySS = 8.46;
+                        }
+                        for (int rowIndex = 5; rowIndex < numRowsInput; rowIndex++)
                         {
                             cellInOut = (Excel.Range)sheet.Cells[rowIndex, 1];
                             cellLabel = (Excel.Range)sheet.Cells[rowIndex, 2];
                             strInOut = cellInOut.Value;
+
                             if (strInOut != null && cellLabel.Value != null)
                             {
                                 if (strInOut.ToLower().Contains('i'))
                                 {
-                                    if (countLabel == 5)
-                                    {
-                                        xI = 6.265;
-                                    }
+
                                 
                                     this.corelApp.ActiveDocument.ActiveLayer.CreateArtisticText(xI, yI, cellLabel.Value, (corel.cdrTextLanguage)1033, 0, "Swis721 Cn BT", 7, 0, 0, 0, (corel.cdrAlignment)1).RotateEx(90.0, xI, yI);
                                 }
@@ -194,7 +206,7 @@ namespace DockerTemplateCS1test
                                 //        {
                                 //            xSS = 6.265 + 0.1624 * 7;
                                 //        }
-                                    
+
                                 //        this.corelApp.ActiveDocument.ActiveLayer.CreateArtisticText(xSS, ySS, cellLabel.Value, (corel.cdrTextLanguage)1033, 0, "Swis721 Cn BT", 7, 0, 0, 0, (corel.cdrAlignment)1).RotateEx(90.0, xSS, ySS);
                                 //    }
                                 //    else
@@ -207,7 +219,7 @@ namespace DockerTemplateCS1test
                                 //        {
                                 //            xO = 4.78;
                                 //        }
-                                    
+
                                 //        this.corelApp.ActiveDocument.ActiveLayer.CreateArtisticText(xO, yO, cellLabel.Value, (corel.cdrTextLanguage)1033, 0, "Swis721 Cn BT", 7, 0, 0, 0, (corel.cdrAlignment)1).RotateEx(90.0, xO, yO);
                                 //        xO += 0.1624;
 
@@ -216,15 +228,10 @@ namespace DockerTemplateCS1test
 
                             }
                         }
-                                yI -= 1.65;
+                        yI -= 1.65;
+                        countLabel++;
 
-                                if (countLabel == 5)
-                                {
-                                    yI = 8.46;
-                                    yO = 8.4;
-                                    ySS = 8.46;
-                                }
-                                countLabel++;
+
 
 
                     }
